71c1675e40f50c8286ddc960c81ead55cf677319
scb-school <scb2197@columbia.edu>
2023-02-20 01:13:51 -0500

    first attempt at map interactivity

--- epispread/skeleton.py
+++ epispread/skeleton.py
@@ -2,34 +2,54 @@ import pandas as pd
 import geopandas as gpd
 import country_converter as coco
 import matplotlib.pyplot as plt
+import datetime
+from matplotlib.widgets import Slider
  
 FILE_1 = "./time_series_covid19_confirmed_global.csv"
 FILE_2 = "./WHO-COVID-19-global-data.csv"
+START_DATE = '2020-07-21'
+START_DATETIME = datetime.datetime.strptime(START_DATE, '%Y-%m-%d')
 
-COUNTRY_LOOKUP = {""}
+
+def setup_manager():
+    df = pd.read_csv(
+        FILE_2, delimiter=",")
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    world = world[world.name != "Antarctica"]
+    return df, world
+
+df, world = setup_manager()
 
 #matches names of column 2 to be those of column 1
 def iso2_to_iso3(iso2_codes):
     return coco.convert(names = iso2_codes, to='ISO3')
 
-def main():
-    df = pd.read_csv(
-        FILE_2, delimiter=",")
-    
-    proof_of_concept = df.loc[df['Date_reported'] == '2021-10-01']
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    
-    iso3_codes = iso2_to_iso3(proof_of_concept['Country_code'])
-    proof_of_concept.loc[:,'Country_code_iso3'] = iso3_codes
+def merge_manager(date):
+    single_date_df = df.loc[df['Date_reported'] == date].copy()
+    iso3_codes = iso2_to_iso3(single_date_df['Country_code'])
+    single_date_df.loc[:,'Country_code_iso3'] = iso3_codes
+    merge = pd.merge(left=world, right=single_date_df, left_on='iso_a3', right_on='Country_code_iso3', how='left')
+    merge.plot(column = 'Cumulative_cases', legend='True', missing_kwds={
+            "color": "lightgrey",
+            "edgecolor": "red",
+            "hatch": "///",
+            "label": "Missing values",
+       },)
 
-    proof_of_concept.to_csv(r'./poc_country.csv')
+def update(time_offset):
+    new_datetime = START_DATETIME + datetime.timedelta(days=time_offset)
+    merge_manager(new_datetime.strftime('%Y-%m-%d'))
+    plt.clf()
+    plt.draw_all()
+
+def main():
+    merge_manager(START_DATE)
+    axcolor = 'lightgoldenrodyellow'
+    axfreq = plt.axes([0.2, 0.1, 0.3, 0.03], facecolor=axcolor)
+    stime = Slider(axfreq, 'Time Offset', 0.0, 30.0, valinit=5.0, valstep=1)
     
-    world['iso_a3'].to_csv(r'./world.csv')
+    stime.on_changed(update)
     
-    merge = pd.merge(left=world, right=proof_of_concept, left_on='iso_a3', right_on='Country_code_iso3', how='left')
-    
-    #disease.plot()
-    merge.plot(column = 'Cumulative_cases', legend='True')
     plt.show()
 
 if __name__ == "__main__":
