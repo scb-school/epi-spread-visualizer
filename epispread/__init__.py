e6ebb80838059c485b55a19671e37a7d4222cbd6
Sienna Brent <scb2197@columbia.edu>
2023-03-02 23:33:23 -0500

    Update __init__.py

--- epispread/__init__.py
+++ epispread/__init__.py
@@ -1,2 +1,2 @@
 from ._version import __version__
-from .hello import read_data, slider_setup, iso2_to_iso3, add_iso3, filter_single_date, merge_manager, plot_all
+from .skeleton import read_data, slider_setup, iso2_to_iso3, add_iso3, filter_single_date, merge_manager, plot_all
