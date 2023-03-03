9abf14f6f3d1dd7699195b72998e09b7faf3c989
scb-school <scb2197@columbia.edu>
2023-03-02 21:52:18 -0500

    slider now affects heatmap coloring

--- epispread/skeleton.py
+++ epispread/skeleton.py
@@ -4,12 +4,17 @@ import country_converter as coco
 import matplotlib.pyplot as plt
 import datetime
 from matplotlib.widgets import Slider
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
  
 FILE_1 = "./time_series_covid19_confirmed_global.csv"
 FILE_2 = "./WHO-COVID-19-global-data.csv"
-START_DATE = '2020-07-21'
+START_DATE = '2020-01-21'
 START_DATETIME = datetime.datetime.strptime(START_DATE, '%Y-%m-%d')
-
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.1)
 
 def setup_manager():
     df = pd.read_csv(
@@ -29,28 +34,30 @@ def merge_manager(date):
     iso3_codes = iso2_to_iso3(single_date_df['Country_code'])
     single_date_df.loc[:,'Country_code_iso3'] = iso3_codes
     merge = pd.merge(left=world, right=single_date_df, left_on='iso_a3', right_on='Country_code_iso3', how='left')
-    merge.plot(column = 'Cumulative_cases', legend='True', missing_kwds={
+    
+    return merge.plot(ax = ax, column = 'Cumulative_cases', legend = True, cax = cax, missing_kwds={
             "color": "lightgrey",
             "edgecolor": "red",
             "hatch": "///",
             "label": "Missing values",
        },)
+    
+merge_graph = merge_manager(START_DATE)
+axcolor = 'lightgoldenrodyellow'
+axfreq = plt.axes([0.2, 0.1, 0.3, 0.03], facecolor=axcolor)
+stime = Slider(axfreq, START_DATE, 0.0, 300.0, valinit=0.0, valstep=20)
+    
+    #print(merge.loc[merge["Date_reported"] == date]['Cumulative_cases'])
+    #print(merge.is_empty)
+    
+    #print("af is " + merge.at[merge.loc[(merge["Country_code"] == "AF") & merge["Date_reported"] == date],'Cumulative_cases'])
 
 def update(time_offset):
     new_datetime = START_DATETIME + datetime.timedelta(days=time_offset)
-    merge_manager(new_datetime.strftime('%Y-%m-%d'))
-    plt.clf()
-    plt.draw_all()
-
-def main():
-    merge_manager(START_DATE)
-    axcolor = 'lightgoldenrodyellow'
-    axfreq = plt.axes([0.2, 0.1, 0.3, 0.03], facecolor=axcolor)
-    stime = Slider(axfreq, 'Time Offset', 0.0, 30.0, valinit=5.0, valstep=1)
+    merge_graph = merge_manager(new_datetime.strftime('%Y-%m-%d'))
+    fig.canvas.draw_idle()
+    fig.canvas.flush_events()
     
-    stime.on_changed(update)
-    
-    plt.show()
+stime.on_changed(update)
 
-if __name__ == "__main__":
-    main()
+plt.show()
