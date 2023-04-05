2d4cb3624af7921b0ce877dcd625ac1fe20f5e7d
scb-school <scb2197@columbia.edu>
2023-04-05 10:56:07 -0400

    Generate docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,32 +5,27 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-from recommonmark.transform import AutoStructify
-
-def setup(app):
-    app.add_config_value('recommonmark_config', {
-        'auto_toc_tree_section': 'Contents',
-    }, True)
-    app.add_transform(AutoStructify)
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 project = 'EpiSpread'
 copyright = '2023, Sienna Brent'
 author = 'Sienna Brent'
-release = '0.1.0'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark', 'insegel']
+extensions = ['recommonmark', 'sphinx.ext.autodoc']
 source_suffix = ['.rst', '.md']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'insegel'
+html_theme = 'alabaster'
 html_static_path = ['_static']
