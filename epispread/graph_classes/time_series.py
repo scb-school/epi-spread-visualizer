5775428f6b568033cf2fdfeefcfd8b86dde9338b
scb-school <scb2197@columbia.edu>
2023-05-08 14:59:09 -0400

    preparations for pushing second release

--- epispread/graph_classes/time_series.py
+++ epispread/graph_classes/time_series.py
@@ -10,6 +10,9 @@ class TimeSeries:
         self.indep_col = indep_col
 
     def plot(self):
+        """plot first converts the values in the "date_col" column of the dataframe to datetime,
+        and then calls pandas.pivot_table to plot each of the "line_col" columns by date and their "indep_col" variable.
+        """
         self.df[self.date_col] = pd.to_datetime(self.df[self.date_col])
         pivot = self.df.pivot_table(index=self.date_col, columns=self.line_col, values=self.indep_col, aggfunc='sum')
         pivot.plot()
